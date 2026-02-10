be378ba862cb73f2e00f883246ebdc5f4b2576d4
ksbhoomika390-sudo <ksbhoomika390@gmail.com>
2026-02-10 17:44:22 +0530

    Add files via upload

---
+++ Day4/for_and_while_loop.py
@@ -0,0 +1,10 @@
+# Using for loop
+print("Even numbers using for loop:")
+for i in range(2, 11, 2):
+    print(i)
+# Using while loop
+print("Even numbers using while loop:")
+i = 2
+while i <= 10:
+    print(i)
+    i += 2
