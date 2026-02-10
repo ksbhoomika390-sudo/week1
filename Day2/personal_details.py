496c996f2991ddf8087a16d9298ce58837015bdb
ksbhoomika390-sudo <ksbhoomika390@gmail.com>
2026-02-10 17:42:13 +0530

    Add files via upload

---
+++ Day2/personal_details.py
@@ -0,0 +1,13 @@
+name = "Bhoomika"
+age = 22
+course = "MCA"
+college = "UBDT College, Davanagere"
+city = "Shivamogga"
+
+print("Personal Details")
+print("----------------")
+print("Name   :", name)
+print("Age    :", age)
+print("Course :", course)
+print("College:", college)
+print("City   :", city)
